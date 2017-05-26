a95bd89054afaddb270688de8bee1190f8815cb1
Administrator <Administrator@PC201609271954>
2017-05-26 08:33:40 +0800

    ..

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-	$("#container").load("html/development.html");
+	$(".am-nav-pills li").click(function(){
+ 		if(!$(this).hasClass("am-dropdown")){
+ 			$(".am-nav-pills li[class!='am-dropdown am-active']").removeClass("am-active");
+ 			$(this).addClass("am-active");
+ 		}
+ 	});
+	$("#container").load("main.html",function(){
+		$.getScript("lib/assets/js/amazeui.min.js");
+	});
 });
